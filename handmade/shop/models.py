75b6f84c2e3eb5c5ff79fb2df45addea3d19ccbb
Tatianasvet <168006070+Tatianasvet@users.noreply.github.com>
2024-10-28 20:57:33 +0500

    Project restart

--- handmade/shop/models.py
+++ handmade/shop/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Salesman(models.Model):
+    pass
+
+
+class Product(models.Model):
+    pass
+
+
+class Category(models.Model):
+    pass
+
+
